0b3db575c98452c991db9210595174b2cc8d2112
Timothy Henry <tim87henry@gmail.com>
2022-05-25 23:39:40 +1000

    Implemented book delete

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -165,14 +165,44 @@ exports.book_create_post = [
 
 
 // Display book delete form on GET.
-exports.book_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete GET');
+exports.book_delete_get = function(req, res, next) {
+
+    async.parallel({
+        book: function(callback) {
+            Book.findById(req.params.id).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.book==null) { // No results.
+            res.redirect('/catalog/book');
+        }
+        // Successful, so render.
+        res.render('book_delete', { title: 'Delete Book', book: results.book} );
+    });
+
 };
 
+
 // Handle book delete on POST.
-exports.book_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete POST');
+exports.book_delete_post = function(req, res, next) {
+
+    async.parallel({
+        book: function(callback) {
+          Book.findById(req.body.bookid).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        // Success
+        //res.render('book_delete', { title: 'Delete Book', book: results.book} );
+        Book.findByIdAndRemove(req.body.bookid, function deleteBook(err) {
+            if (err) { return next(err); }
+            // Success - go to book list
+            res.redirect('/catalog/books')
+        });
+        
+    });
 };
+
 
 // Display book update form on GET.
 exports.book_update_get = function(req, res, next) {
